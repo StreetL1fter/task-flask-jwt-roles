8eb5981c5d5e392e94d4373f724d34cbc827d5ee
StreetL1fter <ismailmendgaliev2005@mail.ru>
2026-03-20 19:32:30 +0400

    New files and new changes

--- .venv/app/__init__.py
+++ .venv/app/__init__.py
@@ -2,12 +2,22 @@ from flask import Flask,render_template,url_for
 from extensions import db
 from config import Config
 from .auth import auth_bp
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+from .auth.routes import register_routes
+from dotenv import load_dotenv
 
+load_dotenv()
 
 def create_app():
     application = Flask(__name__)
     application.config.from_object(Config)
-    db.init_app(application)
     import app.models
+    db.init_app(application)
+    limiter = Limiter(
+        app = application,
+        key_func=get_remote_address)
+    register_routes(limiter)
     application.register_blueprint(auth_bp,url_prefix='/auth')
     return application
+
--- .venv/app/auth/routes.py
+++ .venv/app/auth/routes.py
@@ -4,9 +4,17 @@ from app.models import User,Role,Blacklistedtoken
 from extensions import db
 from config import Config  
 import jwt
+from flask import current_app
 from datetime import timedelta,datetime,timezone
 from functools import wraps
 from app.services.auth_service import AuthService
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+
+
+
+
+
 
 def require_auth(f):
     @wraps(f)
@@ -76,104 +84,98 @@ def require_permission(permission_name):
         return decorated_function
     return decorator
             
+def register_routes(limiter):
+    @auth_bp.route('/logout',methods=["POST"])
+    def logout():
 
-@auth_bp.route('/logout',methods=["POST"])
-def logout():
+        data = request.headers.get('Authorization').split(' ')[1]
 
-    data = request.headers.get('Authorization').split(' ')[1]
+        token = jwt.decode(data,Config.JWT_SECRET_KEY,algorithm="HS256")
+        
+        jti = token['jti']
 
-    token = jwt.decode(data,Config.JWT_SECRET_KEY,algorithm="HS256")
+        try:
+            if jti is not None:
+                new_entry = Blacklistedtoken(jwt_token_id = jti)
+                db.session.add(new_entry)
+                db.session.commit()
+                return jsonify({"message":"Successfully logged out"}),200
+        except:
+            return jsonify({"message": "500 Internal Server Error"}),500
+
+    @auth_bp.route('/register',methods=["POST"])
+    def register():
+        data = request.get_json()
+        if "email" not in data:
+            return jsonify({'message':'необходимо заполнить email'}),400
+        email = data['email']
+        if "@" not in email:
+            return jsonify({'message':"Email не валиден"}), 400
+        if type(email) != str:
+            return jsonify({'message': "Email должен быть строковым типом данных"})
+        
+        if 'password' not in data:
+            return jsonify({'message': 'Заполните все поля'}),400
+        if 'first_name' not in data or 'last_name' not in data:
+            return jsonify({'message': 'Заполните все поля'}),400
+        
+        
+        password = data['password']
+        first_name = data["first_name"]
+        last_name = data["last_name"]
+
+        if type(first_name) != str or type(last_name) != str:
+            return jsonify({"message": "Должно быть текстом быть текстом"})
+        
+        user,error_message = AuthService.register(email,password,first_name,last_name)
+        
+        if error_message:
+            return jsonify({'message': error_message}), 409
+        
+        return jsonify({'message': 'User registered successful','user_id': user.id}),201 
+        
+    @auth_bp.route('/login',methods=['POST'])
+    @limiter.limit("10 per minute")
+    def login():
+        data = request.get_json()
+        if "email" not in data or "password" not in data:
+            return jsonify({'message': 'Email и пароль - обязательны'}), 400
+            
+        email = data["email"]
+        password = data["password"]
+        exists = User.query.filter_by(email=email).first()
+
+        if not exists:
+            return jsonify({"message": "Пользователь не найден"}),404
+        if not exists.check_password(password):
+            return jsonify({"message": "Неверный логин или пароль"}),401
+            
+
+        payload = {
+            'user_id': exists.id,
+            'exp': datetime.now(timezone.utc)+ timedelta(hours=1)
+        }
+        token = jwt.encode(payload,Config.JWT_SECRET_KEY,algorithm="HS256")
+
+        return jsonify({"message":"User successful","token": token}),200
+
+
+
+    @auth_bp.route('/protected',methods=['GET'])
+    @require_auth
+    @require_permission("ismail:read")
+    def get_protected_user(user):
+        all_role_permission = []
+        for role in user.roles:
+            for permission in role.permissions:
+                all_role_permission.append(permission.name)
+        return jsonify({
+            'message': 'Доступ разрешён',
+            "email": user.email,
+            'user_id': user.id,
+            'roles': [role.name for role in user.roles],
+            'permissions': all_role_permission
+        }),200
     
-    jti = token['jti']
-
-    try:
-        if jti is not None:
-            new_entry = Blacklistedtoken(jwt_token_id = jti)
-            db.session.add(new_entry)
-            db.session.commit()
-            return jsonify({"message":"Successfully logged out"}),200
-    except:
-        return jsonify({"message": "500 Internal Server Error"}),500
-
-   
-    
-
-
-
-@auth_bp.route('/register',methods=["POST"])
-def register():
-    data = request.get_json()
-    if "email" not in data:
-        return jsonify({'message':'необходимо заполнить email'}),400
-    email = data['email']
-    if "@" not in email:
-        return jsonify({'message':"Email не валиден"}), 400
-    if type(email) != str:
-        return jsonify({'message': "Email должен быть строковым типом данных"})
-    
-    if 'password' not in data:
-        return jsonify({'message': 'Заполните все поля'}),400
-    if 'first_name' not in data or 'last_name' not in data:
-        return jsonify({'message': 'Заполните все поля'}),400
-    
-    
-    password = data['password']
-    first_name = data["first_name"]
-    last_name = data["last_name"]
-
-    if type(first_name) != str or type(last_name) != str:
-        return jsonify({"message": "Должно быть текстом быть текстом"})
-    
-    user,error_message = AuthService.register(email,password,first_name,last_name)
-    
-    if error_message:
-        return jsonify({'message': error_message}), 409
-    
-    return jsonify({'message': 'User registered successful','user_id': user.id}),201 
-      
-
-
-@auth_bp.route('/login',methods=['POST'])
-
-def login():
-    data = request.get_json()
-    if "email" not in data or "password" not in data:
-        return jsonify({'message': 'Email и пароль - обязательны'}), 400
-    
-    email = data["email"]
-    password = data["password"]
-    exists = User.query.filter_by(email=email).first()
-
-
-    if not exists:
-        return jsonify({"message": "Пользователь не найден"}),404
-    if not exists.check_password(password):
-        return jsonify({"message": "Неверный логин или пароль"}),401
-    
-
-    payload = {
-        'user_id': exists.id,
-        'exp': datetime.now(timezone.utc)+ timedelta(hours=1)
-    }
-    token = jwt.encode(payload,Config.JWT_SECRET_KEY,algorithm="HS256")
-
-    return jsonify({"message":"User successful","token": token}),200
-
-
-
-@auth_bp.route('/protected',methods=['GET'])
-@require_auth
-@require_permission("ismail:read")
-def get_protected_user(user):
-    all_role_permission = []
-    for role in user.roles:
-        for permission in role.permissions:
-            all_role_permission.append(permission.name)
-    return jsonify({
-        'message': 'Доступ разрешён',
-        "email": user.email,
-        'user_id': user.id,
-        'roles': [role.name for role in user.roles],
-        'permissions': all_role_permission
-    }),200
+    return auth_bp
 
